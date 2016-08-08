1ef98f6c829297374dcefedb9329d509ac20128c
TruongPD_GMO <dinhtruong411@gmail.com>
2016-08-08 15:23:34 +0700

    100% rebuild index.html

--- Web-Responsive/Rebuild/js/scoll.js
+++ Web-Responsive/Rebuild/js/scoll.js
@@ -1,21 +1,20 @@
 $(function() {
-	    var topBtn = $('#page-top');   
-	    topBtn.hide();
-	    //ƒXƒNƒ[ƒ‹‚ª100‚É’B‚µ‚½‚çƒ{ƒ^ƒ“•\Ž¦
-	    $(window).scroll(function () {
-	        if ($(this).scrollTop() > 100) {
-	            topBtn.fadeIn();
-	        } else {
-	            topBtn.fadeOut();
-	        }
-	    });
-	    //ƒXƒNƒ[ƒ‹‚µ‚Äƒgƒbƒv
-	    topBtn.click(function () {
-	        $('body,html').animate({
-	            scrollTop: 0
-	        }, 500);
-	        return false;
-	    });
-		
-});
+        var topBtn = $('#top_page_btn');
+        topBtn.hide();
+        //ƒXƒNƒ[ƒ‹‚ª100‚É’B‚µ‚½‚çƒ{ƒ^ƒ“•\Ž¦
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                topBtn.fadeIn();
+            } else {
+                topBtn.fadeOut();
+            }
+        });
+        //ƒXƒNƒ[ƒ‹‚µ‚Äƒgƒbƒv
+        topBtn.click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
 
+});
