c39e89864cd4908131f8d9f7e056f995f02ed6b4
dinhtruong411 <dinhtruong411@gmail.com>
2016-08-23 09:27:02 +0700

    100% code clearn

--- Ruby/Demo1/team1.rb
+++ Ruby/Demo1/team1.rb
@@ -8,15 +8,15 @@
 
 load 'team_manager.rb'
 # Name list
-names_hash = { 'Leader'         => 'Pham Dinh Truong', \
-               'Deputy Leader'  => 'Dang Thi Hoa', \
-               'Member1'        => 'Nguyen Thi Van', \
-               'Member2'        => 'Trinh Thi Tuyet Diem', \
+names_hash = { 'Leader'         => 'Pham Dinh Truong',
+               'Deputy Leader'  => 'Dang Thi Hoa',
+               'Member1'        => 'Nguyen Thi Van',
+               'Member2'        => 'Trinh Thi Tuyet Diem',
                'Member3'        => 'Nguyen Thi Hong Van'
              }
 
 # Date of birth list
-dobs_array = ['13/02/1993', '19/11/1992', '02/10/1992', '12/05/1993', \
+dobs_array = ['13/02/1993', '19/11/1992', '02/10/1992', '12/05/1993',
               '09/08/1993']
 
 project_team = Team_Manager.new(names_hash, dobs_array)
